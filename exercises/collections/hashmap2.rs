0c26552b9b6c55ac35affbb244d97f063e8e009f
Noel Campbell <noelcampbell@microsoft.com>
2021-02-13 01:07:44 -0500

    Worked through arc1.rs

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,11 +12,10 @@
 // Execute the command `rustlings hint collections4` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Debug)]
 enum Fruit {
     Apple,
     Banana,
@@ -38,6 +37,17 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        basket.entry(fruit).or_insert(2);
+        // match fruit {
+        //     Fruit::Apple => println!("No more apples allowed!"),
+        //     Fruit::Lichi => println!("No more lichi allowed!"), 
+        //     Fruit::Mango => println!("No more mangos allowed!"),
+        //     _ => {
+        //         println!("Added 2 {:?} to the basket", fruit);
+        //         basket.insert(fruit, 2);
+        //         ()
+        //     }
+        // }
     }
 }
 
--- exercises/error_handling/errorsn.rs
+++ exercises/error_handling/errorsn.rs
@@ -17,18 +17,19 @@
 //
 // Execute `rustlings hint errorsn` for hints :)
 
-// I AM NOT DONE
-
 use std::error;
 use std::fmt;
 use std::io;
 
 // PositiveNonzeroInteger is a struct defined below the tests.
-fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, ???> {
+fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
     let mut line = String::new();
-    b.read_line(&mut line);
-    let num: i64 = line.trim().parse();
-    let answer = PositiveNonzeroInteger::new(num);
+    b.read_line(&mut line)?;
+    let num: i64 = line.trim().parse()?;
+    let answer: Result<PositiveNonzeroInteger, Box<dyn error::Error>> = match (PositiveNonzeroInteger::new(num)) {
+        Ok(ans) =>  Ok(ans),
+        Err(e) => Err(Box::new(e)),
+    };
     answer
 }
 
